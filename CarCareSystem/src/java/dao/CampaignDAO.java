bb2eac4f95c98955c4700d59e2ea191bb3647b27
ntn_carameo <nguyenthanhnamidol@gmail.com>
2025-06-25 16:49:24 +0700

    add blog and campaign
    add blog and campaign

---
+++ CarCareSystem/src/java/dao/CampaignDAO.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.Campaign;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author NTN
+ */
+public class CampaignDAO extends DBConnection {
+
+    public void addCampaign(Campaign campaign) {
+        String sql = "INSERT INTO [dbo].[Campaign] ([name], [status], [description], [startDate], [endDate]) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement ptm = connection.prepareStatement(sql)) {
+            ptm.setString(1, campaign.getName());
+            ptm.setBoolean(2, campaign.isStatus());
+            ptm.setString(3, campaign.getDescription());
+            ptm.setDate(4, (Date) campaign.getStartDate());
+            ptm.setDate(5, (Date) campaign.getEndDate());
+            ptm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public List<Campaign> getAllCampaigns() {
+        List<Campaign> list = new ArrayList<>();
+        String sql = "SELECT [id], [name], [status], [description], [startDate], [endDate] FROM [dbo].[Campaign]";
+
+        try {
+            PreparedStatement ptm = connection.prepareStatement(sql);
+            ResultSet rs = ptm.executeQuery();
+            while (rs.next()) {
+                Campaign campaign = new Campaign(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getBoolean("status"),
+                        rs.getString("description"),
+                        rs.getDate("startDate"),
+                        rs.getDate("endDate"));
+                list.add(campaign);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+
+    public void updateCampaign(Campaign campaign) {
+        String sql = "UPDATE [dbo].[Campaign] SET [name] = ?, [status] = ?, [description] = ?, [startDate] = ?, [endDate] = ? WHERE [id] = ?";
+
+        try (PreparedStatement ptm = connection.prepareStatement(sql)) {
+            ptm.setString(1, campaign.getName());
+            ptm.setBoolean(2, campaign.isStatus());
+            ptm.setString(3, campaign.getDescription());
+            ptm.setDate(4, (Date) campaign.getStartDate());
+            ptm.setDate(5, (Date) campaign.getEndDate());
+            ptm.setInt(6, campaign.getId()); // điều kiện WHERE theo id
+
+            ptm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void deleteCampaign(int id) {
+        String sql = "DELETE FROM [dbo].[Campaign] WHERE [id] = ?";
+
+        try (PreparedStatement ptm = connection.prepareStatement(sql)) {
+            ptm.setInt(1, id);
+            ptm.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("getAllCampaigns");
+        CampaignDAO instance = new CampaignDAO();
+        List<Campaign> result = instance.getAllCampaigns();
+        System.out.println(result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+}
