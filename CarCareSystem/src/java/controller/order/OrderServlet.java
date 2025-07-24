71c033a447b44f047cebed59096ffa353e6d5423
Tranthanhhai281 <53221090+Tranthanhhai281@users.noreply.github.com>
2025-07-25 05:53:00 +0700

    update to main

--- CarCareSystem/src/java/controller/order/OrderServlet.java
+++ CarCareSystem/src/java/controller/order/OrderServlet.java
@@ -28,9 +28,13 @@ import jakarta.servlet.http.HttpSession;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import java.util.ArrayList;
 import java.util.List;
 import util.SendMailService;
+=======
+import java.text.DecimalFormat;
+>>>>>>> Stashed changes
 =======
 import java.text.DecimalFormat;
 >>>>>>> Stashed changes
@@ -344,8 +348,11 @@ public class OrderServlet extends HttpServlet {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 response.sendRedirect("GenerateQRCode?orderId=" + orderId
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
